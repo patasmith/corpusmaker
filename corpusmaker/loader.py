23e713973e8463b6a1610660bc424f91d54b5b2e
Pat Smith <pat.a.smith.dev@gmail.com>
2024-03-26 20:14:50 -0400

    corrected types; added third test textfile

--- corpusmaker/loader.py
+++ corpusmaker/loader.py
@@ -2,22 +2,24 @@
 Textfile operations for Corpusmaker
 """
 
+from typing import Optional
+
 from dataclasses import dataclass
-from corpusmaker.database import Database
 from corpusmaker.model import RawText
 from loguru import logger
 
 
 @dataclass
 class Loader:
-    def read_file(self, filename: str, separator: str = "") -> None:
+    def read_file(self, filename: str, separator: str = "") -> Optional[RawText]:
         try:
             with open(filename, "r") as f:
                 content = f.read()
-                return RawText(content=content, separator=separator) 
+                return RawText(content=content, separator=separator)
         except FileNotFoundError:
             logger.error(f"{filename} was not found.")
         except UnicodeDecodeError:
             logger.error(f"{filename} is not readable as text.")
         except Exception as e:
             logger.error(e)
+        return None
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,15 +1,23 @@
 from corpusmaker.loader import Loader
-    
+
 
 # Can add single textfile to table
 def test_add_single_textfile_to_table(loader: Loader) -> None:
     raw_text = loader.read_file("tests/files/test_file_1.txt")
+    assert raw_text is not None
     with open("tests/files/test_file_1.txt") as f:
         assert raw_text.content == f.read()
     raw_text = loader.read_file("tests/files/test_file_2.txt", "* * * * *")
+    assert raw_text is not None
     with open("tests/files/test_file_2.txt") as f:
         assert raw_text.content == f.read()
         assert raw_text.separator == "* * * * *"
+    raw_text = loader.read_file("tests/files/test_file_3.txt", "^\n*.*[^.!?]\n^\n")
+    assert raw_text is not None
+    with open("tests/files/test_file_3.txt") as f:
+        assert raw_text.content == f.read()
+        assert raw_text.separator == "^\n*.*[^.!?]\n^\n"
+
 
 # Multiple textfiles can be added at once
 # Each textfile will be a separate entry
